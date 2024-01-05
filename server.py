fcf3308ed4a86dd22893a1a9e5d66e8686903618
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2024-01-05 14:48:53 -0300

    put method

---
+++ server.py
@@ -0,0 +1,43 @@
+# uvicorn server:app --reload 
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get('/')
+
+
+# async def root():
+#     return {"mensagem":"hello"}
+
+# @app.get('/profile')
+# def profile():
+#     return{"nome": "teste"}
+
+# @app.put('/profile')
+# def atualizar():
+#     return{"mensagem": "Perfil Criado com sucesso!"}
+
+# @app.post('/profile')
+# def signup():
+#     return{"mensagem": "Perfil Criado com sucesso!"}
+
+# @app.delete('/profile')
+# def remover():
+#     return{"mensagem": "Perfil Criado com sucesso!"}
+
+@app.get('/saudacao/{nome}')
+def saudacao(nome: str):
+    texto = f'Olá {nome}'
+    return {"mensagem": texto}
+
+@app.get('/quadrado/{numero}')
+def quadrado(numero: int):
+    resultado = numero * numero
+    texto = f' O quadrado de {numero} é {resultado}'
+
+    return {'mensagem': texto}
+
+@app.post('/produtos')
+def produtos():
+    return {'mensaem':'Produto (Espetinho) cadastrado com sucesso'}
